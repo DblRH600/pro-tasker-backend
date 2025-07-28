5ef15bd0dcd9d9a3923a9fd905012420d473de74
Rico H <hrr600@gmail.com>
2025-07-28 11:21:15 -0400

    added models for pro-tasker project

---
+++ models/Project.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose'
+
+const projectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  tasks: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Task'
+  }],
+  projectDueDate: {
+    type: Date,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Project = mongoose.model('Project', projectSchema)
+
+export default Project
---
+++ models/Task.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from 'mongoose'
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['To Do', 'In Progress', 'Done'],
+    default: 'To Do'
+  },
+  project: {
+    type: Schema.Types.ObjectId,
+    ref: 'Project'
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  taskDueDate: {
+    type: Date,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const Task = mongoose.model('Task', taskSchema)
+
+export default Task
